58b6428f6d478afbd7a28f281c9541986a34db71
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-21 15:18:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@
 def turn_count(board)
     board.each do |play|
         counter = 0
-        until play != "X" or play != "O"
+        if play == "X" or play == "O"
             counter += 1
-        return counter
+        end
     end
     end
 end
